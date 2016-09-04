c89dc5a304f7af39a2ef23cbbe9ff2572e3ba5ce
Antsybordzmitry <aviations1983>
2016-09-04 10:24:38 +0300

    implemented bean instantiation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,24 +3,32 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
+
         ApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
+
+        // instantiate bean by name
         ShopLauncher s1 = (ShopLauncher)context.getBean("shopLauncher");
+
+        // instantiate bean by type
         ShopLauncher s2 = (ShopLauncher)context.getBean(ShopLauncher.class);
+
+        // instantiate bean by name and type
         ShopLauncher s3 = (ShopLauncher)context.getBean("shopLauncher",ShopLauncher.class );
+
+        // instantiate bean by alias
         ShopLauncher s4 = (ShopLauncher)context.getBean("my-alias");
 
-        s1.setShopName("123");
         s1.main();
-        s2.setShopName("456");
-        s2.main();
-        s3.setShopName("789");
-        s3.main();
-        s4.setShopName("10.11.12");
-        s4.main();
 
-        System.out.println(s1.getShopName());
-        System.out.println(s2.getShopName());
-        System.out.println(s3.getShopName());
-        System.out.println(s4.getShopName());}
+        s2.setShopName("Amazon");
+        s2.main();
+
+        s3.setShopName("Ebay");
+        s3.main();
+
+        s4.setShopName("Walmart");
+        s4.main();
+    }
 }
